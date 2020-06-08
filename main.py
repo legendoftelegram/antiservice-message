d683ea418f4d499aa5b5cd8f64a76690ad3a2468
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-07 22:07:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,14 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
     app = pyrogram.Client(
         "deleter",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
+        plugins=plugins
     )
     app.run()
-    
-pyrogram.Client.on_message(pyrogram.Filters.service)
-async def service(b:bot, u:update):
-    await b.delete_messages(chat_id=u.chat.id, message_ids=u.message_id)
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
-async def start(b:bot, u:update):
-    await b.send_message(chat_id=u.chat.id, text="**im an anti-service message deletor**"
-    )    
