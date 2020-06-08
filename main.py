cb7f4f685863dcffd4039b81d9631d76bcd07ed7
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-07 21:36:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__" :
     app = pyrogram.Client(
         "deleter",
         bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
+        api_id=Config.API_ID,
         api_hash=Config.API_HASH,
     )
     app.run()
